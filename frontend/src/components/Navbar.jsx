c1c6df416f143b02143e2cc898e41a636ff56ea7
shemanto27 <shemantosharkarofficial@gmail.com>
2025-03-24 21:00:45 +0600

    Power Generation page edited

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { TiThMenu } from "react-icons/ti";
 import SolarModeling from './SolarModeling';
+import { FaInfoCircle } from "react-icons/fa";
 
 
 function Navbar() {
@@ -9,6 +10,18 @@ return (
         <div className="navbar bg-base-100 shadow-sm mb-5">
             <label htmlFor="my-drawer" className="btn btn-primary drawer-button mr-2"><TiThMenu /></label>
             <a className="btn btn-ghost text-xl">Energy Lab: Solar</a>
+            <sup className='text-rose-600 flex justify-center items-center text-sm gap-1 btn' onClick={()=>document.getElementById('my_modal_3').showModal()}><FaInfoCircle />Beta Version</sup>
+
+            <dialog id="my_modal_3" className="modal">
+            <div className="modal-box">
+                <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+                <p className="py-4">This version of the software is available only for testing and showcasing purpose to gather user feedback. Many of functionalities may not work properly, wait till stable version release</p>
+            </div>
+            </dialog>
+            
         </div>
 
         <div className="drawer">
